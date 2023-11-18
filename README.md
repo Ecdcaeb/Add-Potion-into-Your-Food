0d70b3ed6dfbff4357e9e16d52cae55b49b728f2
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2023-11-18 12:51:01 +0800

    clean

--- README.md
+++
@@ -1,5 +0,0 @@
-# Add-Potion-into-Your-Food
-mcmod 1.19.4 forge
-
-try twice but fail twice
-f**k 1.19.4.1.20.2 better
--- try1/untitled/src/main/java/com/Hileb/add_potion/network/IMessage.java
+++
@@ -1,8 +0,0 @@
-package com.Hileb.add_potion.network;
-
-import net.minecraft.network.FriendlyByteBuf;
-
-public interface IMessage {
-    void read(FriendlyByteBuf buf,IMessage t);
-    void write(FriendlyByteBuf buf,IMessage t);
-}
